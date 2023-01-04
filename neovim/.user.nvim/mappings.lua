6310f78abc6f7b289fec6794cb578abdd263d47f
Faiz Chaudhry <faiz.chaudhry97@outlook.com>
2023-01-04 17:43:21 -0500

    created neovim dir

---
+++ neovim/.user.nvim/mappings.lua
@@ -0,0 +1,39 @@
+local fzf = require('fzf-lua')
+
+require('legendary').keymaps({
+  { '<Space>', '<nop>' },
+
+  -- Fast saving
+  { '<leader>w', ':<C-u>silent update<cr>' },
+
+  -- Make j and k behave like they should for wrapped lines
+  { 'j', 'gj' },
+  { 'k', 'gk' },
+
+  -- Buffer navigation keybinds
+  { '<leader>k', function() require('bufdelete').bufwipeout(0) end },
+  { '<leader>b', alt_buf_with_fallback },
+  { '<C-^>', alt_buf_with_fallback },
+
+
+  { '<leader>n', narrow_to_function },
+
+  -- Don't lose visual selection with < >
+  { '<', '<gv', mode = { 'x' } },
+  { '>', '>gv', mode = { 'x' } },
+
+  -- Format
+  { '<leader>F', ':Neoformat<cr>', mode = { 'n', 'v' } },
+
+  -- Fzf
+  { '<leader>f', fzf.files },
+  { '<leader>m', fzf.buffers },
+  { '<leader>g', fzf.live_grep },
+  { '<leader>r', fzf.resume },
+  { '<leader>e', function() fzf.files({ cwd = get_buf_dir()}) end },
+  { '<M-x>', fzf.commands },
+
+  -- Help
+  { '<C-h>f', fzf.help_tags },
+  { '<C-h>k', '<cmd>Legendary<cr>' },
+})
--- neovim/.user.nvim/options.lua
+++ neovim/.user.nvim/options.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = ';'
-
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.relativenumber = false
