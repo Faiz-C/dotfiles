223fc1a993e7a44fa37cd7347a75e2961ce06c36
Faiz Chaudhry <faiz.chaudhry97@outlook.com>
2023-01-15 01:10:59 -0500

    adding in .ideavimrc to make jetbrains function just like neovim (for the most part)

--- neovim/.user.nvim/mappings.lua
+++ neovim/.user.nvim/mappings.lua
@@ -13,8 +13,8 @@ require('legendary').keymaps({
   -- Buffer navigation keybinds
   { '<leader>k', '<cmd>bd<cr>' },
   { '<leader>n', narrow_to_function },
-  { '<leader>ll', '<cmd>bn<cr>' },
-  { '<leader>hh', '<cmd>bp<cr>' },
+  { '<leader>l', '<cmd>bn<cr>' },
+  { '<leader>h', '<cmd>bp<cr>' },
 
   -- Don't lose visual selection with < >
   { '<', '<gv', mode = { 'x' } },
