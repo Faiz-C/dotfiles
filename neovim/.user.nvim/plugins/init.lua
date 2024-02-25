e3ff6d5422e2c40a644864b050919f1f55a68d4b
Faiz-C <faiz.chaudhry97@outlook.com>
2024-02-25 14:37:06 -0500

    update keybinds

--- neovim/.user.nvim/plugins/init.lua
+++ neovim/.user.nvim/plugins/init.lua
@@ -209,7 +209,7 @@ return {
         },
       }
 
-      vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+      vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
       vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
